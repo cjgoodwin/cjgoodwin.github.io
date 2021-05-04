4d7fbbb2acb8ef0052c7b9d046831324abcb0e21
CJ Goodwin <67801269+cjgoodwin@users.noreply.github.com>
2021-05-04 00:12:49 -0500

    try again

--- WDD 230 Week 1/js/index.js
+++ WDD 230 Week 1/js/index.js
@@ -1,10 +1,5 @@
 document.getElementById('lastmod').textContent = document.lastModified;
 // for the copyright date
-document.addEventListener('DOMContentLoaded', function() {
-    // get the initial span element in the html
-    let yearSpan = document.querySelector('#currentYear');
-
-    let currentyear = new Date().getFullYear();
-    // set the elements equal to eachother so we see it on screen
-    yearSpan.textContent = currentyear;
-});
+let year = document.querySelector('#currentyear');
+let currentyear = new Date().getFullYear();
+year.textContent = currentyear;
