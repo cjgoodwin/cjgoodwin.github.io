a47bf21ac228652afd7de72f31bc258d2de4f3bb
CJ Goodwin <67801269+cjgoodwin@users.noreply.github.com>
2021-05-04 00:20:23 -0500

    next

--- WDD 230 Week 1/js/index.js
+++ WDD 230 Week 1/js/index.js
@@ -1,5 +1,5 @@
-document.getElementById('lastmod').textContent = document.lastModified;
+document.getElementById("lastmod").textContent = document.lastModified;
 // for the copyright date
-let year = document.querySelector('#currentyear');
+let year = document.querySelector("#currentyear");
 let currentyear = new Date().getFullYear();
 year.textContent = currentyear;
