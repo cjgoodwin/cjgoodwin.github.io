6b3d755613eee5cdd4d73e6c7c942b671e83acfd
CJ Goodwin <67801269+cjgoodwin@users.noreply.github.com>
2021-05-04 00:03:11 -0500

    current year

--- WDD 230 Week 1/js/index.js
+++ WDD 230 Week 1/js/index.js
@@ -1,3 +1,10 @@
 document.getElementById('lastmod').textContent = document.lastModified;
-let currentyear = getFullYear();
-document.querySelector('#currentyear').textContent = currentyear;
+// for the copyright date
+document.addEventListener('DOMContentLoaded', function() {
+    // get the initial span element in the html
+    let yearSpan = document.querySelector('#currentYear');
+
+    let currentyear = new Date().getFullYear();
+    // set the elements equal to eachother so we see it on screen
+    yearSpan.textContent = currentyear;
+});
