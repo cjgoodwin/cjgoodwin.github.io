3562b048093afb1a0de9864345449a553c3a7eb5
CJ Goodwin <67801269+cjgoodwin@users.noreply.github.com>
2021-05-23 01:19:20 -0500

    initial commit

---
+++ lesson3/js/footer.js
@@ -0,0 +1,5 @@
+document.getElementById("lastmod").textContent = document.lastModified;
+// for the copyright date
+let year = document.querySelector("#currentyear");
+let currentyear = new Date().getFullYear();
+year.textContent = currentyear;
